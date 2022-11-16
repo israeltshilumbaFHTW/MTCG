05d2e102ff5e29747dcb118b3af75656b2e94cc0
isrealtshilumba <if21b088@technikum-wien.at>
2022-11-16 18:02:51 +0100

    getUser function erstellt

--- src/main/java/at/fhtw/sampleapp/service/user/RepoUser.java
+++ src/main/java/at/fhtw/sampleapp/service/user/RepoUser.java
@@ -12,22 +12,70 @@ import java.util.List;
 public class RepoUser {
     private Connection connection = DatabaseConnection.getDatabaseConnection();
 
-    public List<User> getAllUsers() throws SQLException {
-        PreparedStatement statement = connection.prepareStatement(
-                "SELECT * FROM players"
-        );
-        ResultSet queryResult = statement.executeQuery();
+    public User getUser(int user_id) {
+        User user = null;
+        try {
 
-        List<User> userList = null;
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT * FROM players WHERE user_id=?"
+            );
 
-        while (queryResult.next()) {
-            User user = new User(queryResult.getInt(1),
+            statement.setInt(1, user_id);
+            ResultSet queryResult = statement.executeQuery();
+            user = new User(
+                    queryResult.getInt(1),
                     queryResult.getString(2),
                     queryResult.getString(3),
-                    queryResult.getInt(4));
-            userList.add(user);
+                    queryResult.getInt(4)
+            );
+        } catch (SQLException e) {
+            System.err.println("Fehler bei der DB Query" );
+            e.printStackTrace();
         }
+        return user;
+    }
 
+    public User getUser(String user_name) {
+        User user = null;
+        try {
+
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT * FROM players WHERE user_id=?"
+            );
+
+            statement.setString(1, user_name);
+            ResultSet queryResult = statement.executeQuery();
+            user = new User(
+                    queryResult.getInt(1),
+                    queryResult.getString(2),
+                    queryResult.getString(3),
+                    queryResult.getInt(4)
+            );
+        } catch (SQLException e) {
+            System.err.println("Fehler bei der DB Query" );
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+    public List<User> getAllUsers() {
+        List<User> userList = null;
+        try {
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT * FROM players"
+            );
+            ResultSet queryResult = statement.executeQuery();
+
+            while (queryResult.next()) {
+                User user = new User(queryResult.getInt(1),
+                        queryResult.getString(2),
+                        queryResult.getString(3),
+                        queryResult.getInt(4));
+                userList.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return userList;
     }
 }
